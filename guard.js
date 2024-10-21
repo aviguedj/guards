16ee2cfec43229dfd2dedfe7eba80486d7abc0e2
aviguedj <aviguedj555@gmail.com>
2024-10-21 18:07:41 +0300

    modify e.p
    modify e.p (add-point,get-points)

--- guard.js
+++ guard.js
@@ -11,21 +11,19 @@ app.use(cors());
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 
-let guards = [];
+let guardPoints = [];
 
-app.get('/points', (req, res) => {
-    res.json(guards);
-});
 
 app.post('/add-point', (req, res) => {
     const { pointName, location } = req.body;
-
-    const newPoint = { id: guards.length + 1, pointName, location };
-    guards.push(newPoint);
-
-    res.status(200).json({ message: 'Guard point added successfully', point: newPoint });
+    const newPoint = { pointName, location };
+    guardPoints.push(newPoint);
+    res.json({ point: newPoint });
 });
 
+app.get('/get-points', (req, res) => {
+    res.json({ points: guardPoints });
+});
 
 
 
